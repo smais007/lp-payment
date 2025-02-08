c7f6a9bf12036d9fe4001514d81191c35d083304
smais007 <smaisshawon@gmail.com>
2025-02-09 02:39:19 +0600

    basice user routes

---
+++ routes/user.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const userRouter = Router();
+
+userRouter.get("/", (req, res) => res.send({ title: "GET all users" }));
+
+userRouter.get("/:id", (req, res) => res.send({ title: "GET  users details" }));
+
+userRouter.post("/", (req, res) => res.send({ title: "CREATE new user" }));
+
+userRouter.put("/:id", (req, res) => res.send({ title: "UPDATE users" }));
+
+userRouter.delete("/:id", (req, res) => res.send({ title: "DELET user" }));
