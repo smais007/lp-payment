3de2764cd5c80a12297b03dbaf0c1e016f0ef2d6
smais007 <smaisshawon@gmail.com>
2025-02-09 02:15:18 +0600

    setup enviroment variable

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express";
-
+import { PORT } from "./config/env.js";
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome to API Server");
 });
 
-app.listen(3000, () => {
-  console.log("Server is runing on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is runing on port ${PORT}`);
 });
+
+export default app;
