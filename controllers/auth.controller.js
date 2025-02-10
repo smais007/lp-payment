d9f318f5bf533fc56b55d89e91f3215a5fc60b64
smais007 <smaisshawon@gmail.com>
2025-02-10 23:43:32 +0600

    fix user details api

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -87,6 +87,4 @@ export const signIn = async (req, res, next) => {
   }
 };
 
-export const signOut = (req, res, next) => {
-  // Impliment signOut logic
-};
+export const signOut = (req, res, next) => {};
